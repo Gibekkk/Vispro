61857c3dafb2391cacc7e0523eea74aad5869544
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-09-27 08:29:55 +0800

    merge

--- gilbert_labvispro1/lib/gilbert_vispro1_piramid.dart
+++ gilbert_labvispro1/lib/gilbert_vispro1_piramid.dart
@@ -1,25 +1,26 @@
-import 'dart:io';
+import 'dart:io'; // Mengimpor library dart:io untuk menggunakan fungsi input/output
 
-piramid(int iteratorJ, int length, int lastOrder, var dataArr) {
-  (length > 40) ? length = 40 : length;
-  int i = 0;
-  int order = lastOrder;
-  String text = "";
-  while (i <= iteratorJ) {
-    order = (order > dataArr.length - 1) ? 0 : order;
-    text += (i <= iteratorJ) ? "${dataArr[order]}" : "";
-    order++;
-    i++;
-    text += (i <= iteratorJ) ? " " : "";
+piramid(int iteratorJ, int length, int lastOrder, var dataArr) { // Fungsi piramid dengan parameter iteratorJ, length, lastOrder, dan dataArr
+  (length > 40) ? length = 40 : length; // Membatasi panjang piramid maksimal 40
+  int i = 0; // Inisialisasi variabel i dengan nilai 0
+  int order = lastOrder; // Inisialisasi variabel order dengan nilai lastOrder
+  String text = ""; // Inisialisasi variabel text dengan string kosong
+  while (i <= iteratorJ) { // Looping selama i kurang dari atau sama dengan iteratorJ
+    order = (order > dataArr.length - 1) ? 0 : order; // Mengatur order agar tidak melebihi panjang dataArr
+    text += (i <= iteratorJ) ? "${dataArr[order]}" : ""; // Menambahkan elemen dataArr ke text
+    order++; // Increment order
+    i++; // Increment i
+    text += (i <= iteratorJ) ? " " : ""; // Menambahkan spasi ke text jika i kurang dari atau sama dengan iteratorJ
   }
-  int consoleWidth = length * 2 + 2;
+  int consoleWidth = length * 2 + 2; // Menghitung lebar konsol
 
-  int padding = ((consoleWidth - text.length) / 2).round();
+  int padding = ((consoleWidth - text.length) / 2).round(); // Menghitung padding untuk teks
 
-  String paddedText = ' ' * padding + text;
-  stdout.write("${paddedText}");
-  text = "";
-  stdout.write("\n");
-  if (iteratorJ < length - 1)
-    piramid(iteratorJ + 1, length, order, dataArr);
+  String paddedText = ' ' * padding + text; // Menambahkan padding ke teks
+  stdout.write("${paddedText}"); // Menulis teks yang sudah dipadding ke stdout
+  text = ""; // Mengosongkan variabel text
+  stdout.write("\n"); // Menulis newline ke stdout
+  if (iteratorJ < length - 1) // Jika iteratorJ kurang dari length - 1
+    piramid(iteratorJ + 1, length, order, dataArr); // Memanggil fungsi piramid secara rekursif
 }
+
