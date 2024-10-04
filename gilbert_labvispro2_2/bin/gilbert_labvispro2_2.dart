957d565402b6228d83eb55693c60454aa2367f77
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-10-04 10:17:05 +0800

    starter kit kembang api

--- gilbert_labvispro2_2/bin/gilbert_labvispro2_2.dart
+++ gilbert_labvispro2_2/bin/gilbert_labvispro2_2.dart
@@ -1,5 +1,69 @@
-import 'package:gilbert_labvispro2_2/gilbert_labvispro2_2.dart' as gilbert_labvispro2_2;
+import '../lib/colorCode.dart' as color;
+import 'dart:async';
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${gilbert_labvispro2_2.calculate()}!');
+import 'dart:io';
+
+getScreenSize() {
+  return [stdout.terminalColumns, stdout.terminalLines];
+}
+
+void printCoords(String text, int width, int height) {
+  int screenHeight = getScreenSize()[1];
+
+  // Hitung posisi horizontal (di tengah layar)
+  int centerX = width;
+
+  // Hitung posisi vertikal (di tengah layar)
+  int centerY = screenHeight - height;
+
+  // Pindahin kursor ke tengah layar
+  stdout.write('\x1B[${centerY};${centerX}H');
+
+  // Print teks
+  stdout.write(text);
+
+  // Reset posisi kursor ke bawah
+  stdout.write('\x1B[${screenHeight};0H');
+}
+
+void clearScreen() {
+  print("\x1B[2J\x1B[0;0H"); // clear entire screen, move cursor to 0;0
+}
+
+Future<void> delay(int milliseconds) async {
+  await Future.delayed(Duration(milliseconds: milliseconds));
+}
+
+int random(int min, int max) {
+  return min + Random().nextInt(max - min);
+}
+
+int randomMax(int max) {
+  return Random().nextInt(max);
+}
+
+void main() async {
+  clearScreen();
+  String text = "|";
+  stdout.write("Masukkan Jumlah kembang Api: ");
+  int? jumlahKembangApi = (int.parse(stdin.readLineSync()!));
+  jumlahKembangApi = jumlahKembangApi < 1 ? 1 : jumlahKembangApi;
+
+  clearScreen();
+  for (int j = 0; j < jumlahKembangApi; j++) {
+    int minHeight = getScreenSize()[1] ~/ 3;
+    int randomX = randomMax(getScreenSize()[0]);
+    int randomY = random(minHeight, getScreenSize()[1] - minHeight);
+    String randomColor = color.getRandomColor();
+    if(j == 0){
+      randomX = getScreenSize()[0] ~/ 2;
+      randomY = getScreenSize()[1] ~/ 2;
+    }
+    for (int i = 0; i < randomY; i++) {
+      printCoords(randomColor+text+color.RESET, randomX, i);
+      await delay(100);
+      clearScreen();
+    }
+  }
 }
--- gilbert_labvispro2_2/test/gilbert_labvispro2_2_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:gilbert_labvispro2_2/gilbert_labvispro2_2.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
