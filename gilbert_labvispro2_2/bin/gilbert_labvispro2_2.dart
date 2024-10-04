59efdef0591d27a14766f892317fcd88e322837a
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-10-04 09:26:19 +0800

    create project

---
+++ gilbert_labvispro2_2/bin/gilbert_labvispro2_2.dart
@@ -0,0 +1,5 @@
+import 'package:gilbert_labvispro2_2/gilbert_labvispro2_2.dart' as gilbert_labvispro2_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gilbert_labvispro2_2.calculate()}!');
+}
---
+++ gilbert_labvispro2_2/test/gilbert_labvispro2_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:gilbert_labvispro2_2/gilbert_labvispro2_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
