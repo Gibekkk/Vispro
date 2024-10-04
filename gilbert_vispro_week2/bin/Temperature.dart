6a91c814204cf73a8e31607ecc9d6c6be0bb1e53
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-10-04 09:23:56 +0800

    initial commit

--- gilbert_vispro_week2/bin/Temperature.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-import 'package:gilbert_vispro_week2/Temperature.dart';
-void main(){
-  stdout.write("Masukkan Temperatur (Celcius): ");
-  Temperature temperatur = new Temperature(double.parse(stdin.readLineSync()!));
-  temperatur.temperatureConvert();
-}
