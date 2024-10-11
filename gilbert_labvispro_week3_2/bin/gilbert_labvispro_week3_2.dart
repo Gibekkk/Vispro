1c5ef5f1b39ef699e8d72c26452ed78b3e5309d1
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-10-11 09:03:02 +0800

    starter kit print kadal

--- gilbert_labvispro_week3_2/bin/gilbert_labvispro_week3_2.dart
+++
@@ -1,5 +0,0 @@
-import 'package:gilbert_labvispro_week3_2/gilbert_labvispro_week3_2.dart' as gilbert_labvispro_week3_2;
-
-void main(List<String> arguments) {
-  print('Hello world: ${gilbert_labvispro_week3_2.calculate()}!');
-}
---
+++ gilbert_labvispro_week3_2/lib/kadal.dart
@@ -0,0 +1,152 @@
+import 'dart:io';
+
+import 'package:gilbert_labvispro_week3_2/functions.dart' as functions;
+
+void kadalMaker(int panjangBadan, String arah, int xKepala, int yKepala) {
+  int maxX = functions.getScreenSize()[0] - 3;
+  int maxY = functions.getScreenSize()[1] - 3;
+  int minX = 3;
+  int minY = 3;
+  if (xKepala < minX) xKepala = minX;
+  if (xKepala > maxX) xKepala = maxX;
+  if (yKepala < minY) yKepala = minY;
+  if (yKepala > maxY) yKepala = maxY;
+  functions.moveTo(yKepala, xKepala);
+  stdout.write("Q");
+  switch (arah) {
+    case "atas":
+      printKadalAtas(yKepala, xKepala, panjangBadan);
+      break;
+    case "bawah":
+      printKadalBawah(yKepala, xKepala, panjangBadan);
+      break;
+    case "kanan":
+      printKadalKanan(yKepala, xKepala, panjangBadan);
+      break;
+    case "kiri":
+      printKadalKiri(yKepala, xKepala, panjangBadan);
+      break;
+  }
+}
+
+void printKadalAtas(int yKepala, int xKepala, int panjangBadan) {
+  functions.moveTo(yKepala + 1, xKepala);
+  stdout.write("#");
+  functions.moveTo(yKepala + 1, xKepala - 1);
+  stdout.write("#");
+  functions.moveTo(yKepala + 1, xKepala - 2);
+  stdout.write("#");
+  functions.moveTo(yKepala + 1, xKepala + 1);
+  stdout.write("#");
+  functions.moveTo(yKepala + 1, xKepala + 2);
+  stdout.write("#");
+  for (int i = 0; i <= panjangBadan; i++) {
+    functions.moveTo(yKepala + 1 + i, xKepala);
+    stdout.write("#");
+  }
+  functions.moveTo(yKepala + panjangBadan + 2, xKepala);
+  stdout.write("#");
+  functions.moveTo(yKepala + panjangBadan + 2, xKepala - 1);
+  stdout.write("#");
+  functions.moveTo(yKepala + panjangBadan + 2, xKepala - 2);
+  stdout.write("#");
+  functions.moveTo(yKepala + panjangBadan + 2, xKepala + 1);
+  stdout.write("#");
+  functions.moveTo(yKepala + panjangBadan + 2, xKepala + 2);
+  stdout.write("#");
+  functions.moveTo(yKepala + panjangBadan + 3, xKepala);
+  stdout.write("#");
+}
+
+void printKadalBawah(int yKepala, int xKepala, int panjangBadan) {
+  functions.moveTo(yKepala - 1, xKepala);
+  stdout.write("#");
+  functions.moveTo(yKepala - 1, xKepala - 1);
+  stdout.write("#");
+  functions.moveTo(yKepala - 1, xKepala - 2);
+  stdout.write("#");
+  functions.moveTo(yKepala - 1, xKepala + 1);
+  stdout.write("#");
+  functions.moveTo(yKepala - 1, xKepala + 2);
+  stdout.write("#");
+  for (int i = 0; i <= panjangBadan; i++) {
+    functions.moveTo(yKepala - 1 - i, xKepala);
+    stdout.write("#");
+  }
+  functions.moveTo(yKepala - panjangBadan - 2, xKepala);
+  stdout.write("#");
+  functions.moveTo(yKepala - panjangBadan - 2, xKepala - 1);
+  stdout.write("#");
+  functions.moveTo(yKepala - panjangBadan - 2, xKepala - 2);
+  stdout.write("#");
+  functions.moveTo(yKepala - panjangBadan - 2, xKepala + 1);
+  stdout.write("#");
+  functions.moveTo(yKepala - panjangBadan - 2, xKepala + 2);
+  stdout.write("#");
+  functions.moveTo(yKepala - panjangBadan - 3, xKepala);
+  stdout.write("#");
+}
+
+void printKadalKanan(int yKepala, int xKepala, int panjangBadan) {
+  functions.moveTo(yKepala, xKepala - 1);
+  stdout.write("#");
+  functions.moveTo(yKepala - 1, xKepala - 1);
+  stdout.write("#");
+  functions.moveTo(yKepala - 2, xKepala - 1);
+  stdout.write("#");
+  functions.moveTo(yKepala + 1, xKepala - 1);
+  stdout.write("#");
+  functions.moveTo(yKepala + 2, xKepala - 1);
+  stdout.write("#");
+  for (int i = 0; i <= panjangBadan; i++) {
+    functions.moveTo(yKepala, xKepala - 1 - i);
+    stdout.write("#");
+  }
+  functions.moveTo(yKepala, xKepala - panjangBadan - 2);
+  stdout.write("#");
+  functions.moveTo(yKepala - 1, xKepala - panjangBadan - 2);
+  stdout.write("#");
+  functions.moveTo(yKepala - 2, xKepala - panjangBadan - 2);
+  stdout.write("#");
+  functions.moveTo(yKepala + 1, xKepala - panjangBadan - 2);
+  stdout.write("#");
+  functions.moveTo(yKepala + 2, xKepala - panjangBadan - 2);
+  stdout.write("#");
+  functions.moveTo(yKepala, xKepala - panjangBadan - 3);
+  stdout.write("#");
+}
+
+void printKadalKiri(int yKepala, int xKepala, int panjangBadan) {
+  functions.moveTo(yKepala, xKepala + 1);
+  stdout.write("#");
+  functions.moveTo(yKepala - 1, xKepala + 1);
+  stdout.write("#");
+  functions.moveTo(yKepala - 2, xKepala + 1);
+  stdout.write("#");
+  functions.moveTo(yKepala + 1, xKepala + 1);
+  stdout.write("#");
+  functions.moveTo(yKepala + 2, xKepala + 1);
+  stdout.write("#");
+  for (int i = 0; i <= panjangBadan; i++) {
+    functions.moveTo(yKepala, xKepala + 1 + i);
+    stdout.write("#");
+  }
+  functions.moveTo(yKepala, xKepala + panjangBadan + 2);
+  stdout.write("#");
+  functions.moveTo(yKepala - 1, xKepala + panjangBadan + 2);
+  stdout.write("#");
+  functions.moveTo(yKepala - 2, xKepala + panjangBadan + 2);
+  stdout.write("#");
+  functions.moveTo(yKepala + 1, xKepala + panjangBadan + 2);
+  stdout.write("#");
+  functions.moveTo(yKepala + 2, xKepala + panjangBadan + 2);
+  stdout.write("#");
+  functions.moveTo(yKepala, xKepala + panjangBadan + 3);
+  stdout.write("#");
+}
+
+void main() {
+  functions.clearScreen();
+  kadalMaker(2, "kiri", 15, 15);
+  functions.moveTo(functions.getScreenSize()[1], 0);
+}
