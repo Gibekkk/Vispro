40fc983e20bd3260c74bf202313e0a1557dc40ca
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-10-11 08:39:42 +0800

    initial commit

--- gilbert_labvispro_week3/bin/gilbert_labvispro_week3.dart
+++
@@ -1,5 +0,0 @@
-import 'package:gilbert_labvispro_week3/linkedList.dart' as linkedList;
-
-void main(List<String> arguments) {
-  linkedList.main();
-}
---
+++ gilbert_labvispro_week3_2/bin/gilbert_labvispro_week3_2.dart
@@ -0,0 +1,5 @@
+import 'package:gilbert_labvispro_week3_2/gilbert_labvispro_week3_2.dart' as gilbert_labvispro_week3_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gilbert_labvispro_week3_2.calculate()}!');
+}
